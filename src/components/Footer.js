1ee5a984d37901dd14cd128b5f90ca99bf97db79
10Sadique <jafarsjahan@gmail.com>
2022-11-08 22:47:34 +0600

    social media icons added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import FacebookIcon from './icons/FacebookIcon';
+import InstagramIcon from './icons/InstagramIcon';
+import TwitterIcon from './icons/TwitterIcon';
+import YoutubeIcon from './icons/YoutubeIcon';
 
 const Footer = () => {
     return (
@@ -21,8 +25,23 @@ const Footer = () => {
                 </div>
                 <div className="md:block hidden h-[80px] border-l border-gray-light" />
                 <div>
+                    <div className="flex items-center justify-center gap-5">
+                        <div className="grid w-10 h-10 rounded-full cursor-pointer bg-orange/20 place-items-center">
+                            <FacebookIcon />
+                        </div>
+                        <div className="grid w-10 h-10 rounded-full cursor-pointer bg-orange/20 place-items-center">
+                            <TwitterIcon />
+                        </div>
+                        <div className="grid w-10 h-10 rounded-full cursor-pointer bg-orange/20 place-items-center">
+                            <InstagramIcon />
+                        </div>
+                        <div className="grid w-10 h-10 rounded-full cursor-pointer bg-orange/20 place-items-center">
+                            <YoutubeIcon />
+                        </div>
+                    </div>
+                    <div className="mb-5" />
                     <p className="text-center text-gray-light md:text-left">
-                        Coypritht &copy; 2022. Jafar Sadiqu Jahan. All Rights
+                        Coypright &copy; 2022. Jafar Sadiqu Jahan. All Rights
                         Reseved.
                     </p>
                 </div>
--- src/pages/MyReviews.js
+++ src/pages/MyReviews.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import useTitle from '../hooks/useTitle';
 
 const MyReviews = () => {
+    useTitle('My Title');
+
     return <div>MyReviews</div>;
 };
 
