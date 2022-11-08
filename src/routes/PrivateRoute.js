ca880e5dbd35ad67347ff096f57cc94b506d6f02
10Sadique <jafarsjahan@gmail.com>
2022-11-08 19:41:25 +0600

    private route added

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import { AuthContext } from '../contexts/AuthProvider';
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center my-20">
+                <Spinner />
+            </div>
+        );
+    }
+
+    if (!user) {
+        return <Navigate to={'/signin'} state={{ from: location }} replace />;
+    }
+
+    return children;
+};
+
+export default PrivateRoute;
